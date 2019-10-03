802e0575d20563c7ad33cf0bad3b9abeeb75a141
Khan <mdkhan@internal.tavisca.com>
2019-10-03 15:04:18 +0530

    DecoratorEverydayDemo completed

--- src/decorator/DecoratorEverydayDemo.java
+++ src/decorator/DecoratorEverydayDemo.java
@@ -1,7 +1,18 @@
 package decorator;
 
+import java.io.*;
+
 public class DecoratorEverydayDemo {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File file = new File("./output.txt");
+        file.createNewFile();
+
+        OutputStream oStream = new FileOutputStream(file);
+
+        DataOutputStream doStream = new DataOutputStream(oStream);
+        doStream.writeChars("text");
+
+        doStream.close();
+        oStream.close();
     }
 }
